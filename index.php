c08d1a5ed1179db56c9937264b5bdd5e74d65f27
Jayson Liu <52879802+Jaysonliu9988@users.noreply.github.com>
2021-01-19 21:16:51 +0800

    composer update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once ("vendor/autoload.php");
 require_once ("src/php/component.php");
 require_once ("src/php/operation.php");
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '99ee1e49cb0acc5b3cf2d54dc9d58e08b35ba6bd',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '99ee1e49cb0acc5b3cf2d54dc9d58e08b35ba6bd',
+    ),
+  ),
+);
